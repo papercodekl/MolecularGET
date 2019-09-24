bd4342a760471f4fbd16b3733334bc7d4a0ac429
papercodekl <55735580+papercodekl@users.noreply.github.com>
2019-09-24 16:55:59 +0800

    Update trans.sh

--- trans.sh
+++ trans.sh
@@ -1,2 +1,2 @@
-export CUDA_VISIBLE_DEVICES=5
-nohup python -u translate.py -model experiments/checkpoints2/model_step_500000.pt -gpu 0 -src data2/src-test.txt -output pred.txt -beam_size 5 -n_best 5 -batch_size 128 -replace_unk -max_length 200 -fast > trans.out &
+#export CUDA_VISIBLE_DEVICES=5
+nohup python -u translate.py -model experiments/checkpoints2/model_step_300000.pt -gpu 0 -src data2/src-test.txt -output pred.txt -beam_size 10 -n_best 10 -batch_size 512 -replace_unk -max_length 200 -fast > trans.out &
